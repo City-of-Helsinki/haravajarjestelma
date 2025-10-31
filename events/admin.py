180741c0a6ccc0fda8f7fbb9afd339c760514217
Juha Louhiranta <juha.louhiranta@anders.com>
2025-10-31 16:34:05 +0200

    feat: use GISModelAdmin in Django admin
    Refs: PS-210

--- events/admin.py
+++ events/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from django.contrib.gis.admin import OSMGeoAdmin
+from django.contrib.gis.admin import GISModelAdmin
 
 from .models import Event
 
 
 @admin.register(Event)
-class EventAdmin(OSMGeoAdmin):
-    default_lon = 2776460  # Central Railway Station in EPSG:3857
-    default_lat = 8438120
-    default_zoom = 10
+class EventAdmin(GISModelAdmin):
+    gis_widget_kwargs = {
+        "attrs": {
+            "default_zoom": 10,
+            "default_lon": 24.941389,  # Central Railway Station in EPSG:4326,
+            "default_lat": 60.171944,
+        },
+    }
     list_display = (
         "name",
         "start_time",
